b5d6998e8201ed9030da222de859c2262f5e1a18
BLIND Rane <wyatt.karnes@students.svu.edu>
2017-10-13 13:09:50 -0400

    Added a button to the HomeFragment and enabled actions on the welcome text view in the HomeFragment.

--- app/src/main/java/bwastedsoftware/district_7570_conference/HomeFragment.java
+++ app/src/main/java/bwastedsoftware/district_7570_conference/HomeFragment.java
@@ -7,20 +7,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
+    Button btnCurrent;
+    TextView welcomeText;
 
 
 
-    public HomeFragment() {
-        // Required empty public constructor
+
+    public HomeFragment(){
+        // Required empty constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        // Link the layout pieces with their Java counterparts
+        btnCurrent = (Button) view.findViewById(R.id.current_button);
+        welcomeText = (TextView) view.findViewById(R.id.welcomeTxtView);
+
+        //Give abilities to the layout parts
+        btnCurrent.setOnClickListener(this);
+
+
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
