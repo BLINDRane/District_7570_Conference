f42b8c0747f615eb894467697a0b1bfba302faa5
Boston Cartwright <bostonc@gmail.com>
2017-11-13 13:46:08 -0500

    images are now resized before uploaded to firebase storage regarding speaker photos. also added myself to list of admins.

--- app/src/main/java/bwastedsoftware/district_7570_conference/createSpeakerFragment.java
+++ app/src/main/java/bwastedsoftware/district_7570_conference/createSpeakerFragment.java
@@ -1,12 +1,11 @@
 package bwastedsoftware.district_7570_conference;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.media.Image;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +13,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,11 +22,8 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
-import bwastedsoftware.district_7570_conference.dummy.cEvent;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -67,31 +62,7 @@ public class createSpeakerFragment extends Fragment implements View.OnClickListe
         if(v.getId() == R.id.btn_Save_Speaker){
             //create a new User using information put in by the *ahem* user.
 
-            final String name = etSpeakerName.getText().toString().trim();
-            final String bio = etSpeakerBio.getText().toString().trim();
-
-            StorageReference filepath = mStorage.child("SpeakerPics").child(imageURI.getLastPathSegment());
-            //Check for success.
-            //TODO: Check for failure.
-            filepath.putFile(imageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    @SuppressWarnings("VisibleForTests")
-            //Succeeding here means we have first saved the picture to the STORAGE, and then
-            //also saved the new speaker with name, bio, and image URL string to the database.
-                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
-                    DatabaseReference newSpeaker = mDatabase.push();
-                    newSpeaker.child("name").setValue(name);
-                    newSpeaker.child("bio").setValue(bio);
-                    newSpeaker.child("photoURL").setValue(downloadUrl.toString());
-                    //After creating a speaker, the user should be returned to the homeFragment.
-                }
-            });
-
-
-            String key = mDatabase.child("Speakers").push().getKey();
-            //create a new speaker object
-            Speaker speaker = new Speaker(name, bio, imageURI.toString());
+            uploadSpeaker();
 
 
 
@@ -101,6 +72,70 @@ public class createSpeakerFragment extends Fragment implements View.OnClickListe
 
 
     }
+
+    private void uploadSpeaker()
+    {
+        final String name = etSpeakerName.getText().toString().trim();
+        final String bio = etSpeakerBio.getText().toString().trim();
+
+        StorageReference filepath = mStorage.child("SpeakerPics").child(imageURI.getLastPathSegment());
+
+        Bitmap bitmap = null;
+        try
+        {
+            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),imageURI);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Bitmap resized = Bitmap.createScaledBitmap(bitmap, 300, 300, false);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        resized.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = filepath.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                DatabaseReference newSpeaker = mDatabase.push();
+                newSpeaker.child("name").setValue(name);
+                newSpeaker.child("bio").setValue(bio);
+                newSpeaker.child("photoURL").setValue(downloadUrl.toString());
+            }
+        });
+
+        //    //Check for success.
+        //    //TODO: Check for failure.
+        //    filepath.putFile(imageURI).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        //        @Override
+        //        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+        //            @SuppressWarnings("VisibleForTests")
+        //    //Succeeding here means we have first saved the picture to the STORAGE, and then
+        //    //also saved the new speaker with name, bio, and image URL string to the database.
+        //            Uri downloadUrl = taskSnapshot.getDownloadUrl();
+        //            DatabaseReference newSpeaker = mDatabase.push();
+        //            newSpeaker.child("name").setValue(name);
+        //            newSpeaker.child("bio").setValue(bio);
+        //            newSpeaker.child("photoURL").setValue(downloadUrl.toString());
+        //            //After creating a speaker, the user should be returned to the homeFragment.
+        //        }
+        //    });
+
+
+        //String key = mDatabase.child("Speakers").push().getKey();
+        //create a new speaker object
+        //Speaker speaker = new Speaker(name, bio, imageURI.toString());
+    }
+
 
     private void openGallery(){
         //Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
