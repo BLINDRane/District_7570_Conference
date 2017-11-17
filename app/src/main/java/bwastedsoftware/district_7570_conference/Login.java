ab9d3abd39d32ff47a6aad930586df612f803af9
Boston Cartwright <bostonc@gmail.com>
2017-11-16 20:14:05 -0500

    in the login screen, pressing enter on the keyboard after typing your password now clicks Login

--- app/src/main/java/bwastedsoftware/district_7570_conference/Login.java
+++ app/src/main/java/bwastedsoftware/district_7570_conference/Login.java
@@ -6,11 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -74,6 +76,21 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         adminEmails.add("wyatt.karnes@gmail.com");
         adminEmails.add("george.karnes@yahoo.com");
         adminEmails.add("bostonc@gmail.com");
+
+        //enter listener to login
+        etPassword.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.KEYCODE_ENTER)
+                {
+                    checkLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
