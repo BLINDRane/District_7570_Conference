bad0b42a1d507dea61c496d629de5b49bdc32d15
BLIND Rane <wyatt.karnes@students.svu.edu>
2017-11-21 16:31:52 -0500

    Now, when the user goes into their schedule, events that have been ended will flash and change color, so that they know which ones are over and need to be rated.
    Similarly, when a user goes into the full schedule, any events that have ended will have changed color, but they will not flash.

--- app/src/main/java/bwastedsoftware/district_7570_conference/RVAdapter.java
+++ app/src/main/java/bwastedsoftware/district_7570_conference/RVAdapter.java
@@ -4,11 +4,16 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,11 +22,10 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder>{
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder> {
 
 
-    public static class CardViewHolder extends RecyclerView.ViewHolder
-    {
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         TextView eventTitle;
         TextView eventTime;
@@ -29,8 +33,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder>{
         View view;
 
 
-        CardViewHolder(View itemView)
-        {
+        CardViewHolder(View itemView) {
             super(itemView);
             this.view = itemView;
             cv = (CardView) itemView.findViewById(R.id.cv);
@@ -44,25 +47,42 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder>{
     private Context context;
     private ScheduleFragment fragment;
 
-    RVAdapter(List<Event> cards, Context context, ScheduleFragment fragment)
-    {
+    RVAdapter(List<Event> cards, Context context, ScheduleFragment fragment) {
         this.cards = cards;
         this.context = context;
         this.fragment = fragment;
     }
 
-    public void setCards(List<Event> cards)
-    {
+    public void setCards(List<Event> cards) {
         this.cards = cards;
     }
 
     @Override
     public void onBindViewHolder(final CardViewHolder cardViewHolder, final int i) {
+
         cardViewHolder.eventTitle.setText(cards.get(i).getTitle());
         cardViewHolder.eventTime.setText(cards.get(i).getTime());
         Picasso.with(context).load(cards.get(i).getSpeaker().getPhotoURL()).fit().centerCrop().placeholder(R.drawable.ic_account_circle_black_24dp).transform(new PicassoCircleTransform()).into(cardViewHolder.speakerPhoto);
 
-
+        if(fragment.isMine){ //this affects events in mySchedule when they are over, so they change color and
+        for(int a = 0; a <cards.size(); a++) {
+            if (i == a && cards.get(a).isOver()) {
+                Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
+                animation.setDuration(400); // duration - half a second
+                animation.setInterpolator(new DecelerateInterpolator()); // animation slows over time
+                animation.setRepeatCount(5); // Repeat animation 5 times
+                animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+                cardViewHolder.view.startAnimation(animation);
+                cardViewHolder.view.setBackgroundColor(Color.GRAY);
+            }
+        }
+        } else { //this is to affect events that have ended in main schedule. might not want this.
+            for(int a = 0; a <cards.size(); a++) {
+                if (i == a && cards.get(a).isOver()) {
+                    cardViewHolder.view.setBackgroundColor(Color.GRAY);
+                     }
+                }
+            }
 
         cardViewHolder.view.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -96,7 +116,7 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CardViewHolder>{
         cardViewHolder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Log.i("W4K","Click-"+position);
+                cardViewHolder.view.clearAnimation();
                 fragment.loadEventDetails(cards.get(i));
             }
         });
